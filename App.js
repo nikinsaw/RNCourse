8e84260e819e4860c42c4149b33bd79d229fe437
Nikita Sawant <nikita_sawant@persistent.com>
2024-05-10 13:07:35 +0530

    chore: added image to modal and finishing styles

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { StyleSheet, Text, View, FlatList, Button } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
@@ -31,32 +32,34 @@ export default function App() {
   }
 
   return (
-    <View style={styles.appContainer}>
-      <Button
-        title="Add new goal"
-        color="#5E0acc"
-        onPress={startAddGoalHandler}
-      />
-      <GoalInput
-        visible={modalIsVisible}
-        onAddGoal={addGoalHandler}
-        onCancel={endAddGoalHandler}
-      />
-      <View style={styles.goalsContainer}>
-        <Text>List of Goals</Text>
-        <FlatList
-          data={courseGoals}
-          renderItem={({ item }) => (
-            <GoalItem
-              onDeleteItem={deleteGoalHandler}
-              text={item.text}
-              id={item.id}
-            />
-          )}
-          keyExtractor={(item, index) => item.id + index}
+    <>
+      <StatusBar style="light" />
+      <View style={styles.appContainer}>
+        <Button
+          title="Add new goal"
+          color="#5E0acc"
+          onPress={startAddGoalHandler}
         />
+        <GoalInput
+          visible={modalIsVisible}
+          onAddGoal={addGoalHandler}
+          onCancel={endAddGoalHandler}
+        />
+        <View style={styles.goalsContainer}>
+          <FlatList
+            data={courseGoals}
+            renderItem={({ item }) => (
+              <GoalItem
+                onDeleteItem={deleteGoalHandler}
+                text={item.text}
+                id={item.id}
+              />
+            )}
+            keyExtractor={(item, index) => item.id + index}
+          />
+        </View>
       </View>
-    </View>
+    </>
   );
 }
 
@@ -64,8 +67,7 @@ const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
     paddingTop: 50,
-    paddingHorizontal: 16,
-    backgroundColor: "#fff"
+    paddingHorizontal: 16
   },
   goalsContainer: {
     flex: 4
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { View, TextInput, Button, StyleSheet, Modal } from "react-native";
+import {
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  Modal,
+  Image
+} from "react-native";
 
 function GoalInput(props) {
   const [enteredGoalText, setEnteredGoalText] = useState("");
@@ -15,6 +22,10 @@ function GoalInput(props) {
   return (
     <Modal visible={props.visible} animationType="slideIn">
       <View style={styles.inputContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/image/goal.png")}
+        />
         <TextInput
           style={styles.textInput}
           placeholder="your course goal"
@@ -23,10 +34,10 @@ function GoalInput(props) {
         />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
-            <Button title="Add Goal" onPress={addGoalHandler} />
+            <Button title="Add Goal" onPress={addGoalHandler} color="#5e0acc" />
           </View>
           <View style={styles.button}>
-            <Button title="Cancel" onPress={props.onCancel} />
+            <Button title="Cancel" onPress={props.onCancel} color="#f31282" />
           </View>
         </View>
       </View>
@@ -42,13 +53,20 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     padding: 16,
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: "#cccccc"
+    backgroundColor: "#311b6b"
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20
   },
   textInput: {
     borderWidth: 1,
-    borderColor: "#cccccc",
+    borderColor: "#e4d0ff",
+    backgroundColor: "#e4d0ff",
+    borderRadius: 6,
+    padding: 16,
+    color: "#120438",
     width: "100%",
     margin: 8,
     padding: 8
