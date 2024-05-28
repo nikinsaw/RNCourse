d2c42c93072b800c9d445bf653a39e7080de14bb
Nikita Sawant <nikita_sawant@persistent.com>
2024-05-28 18:18:18 +0530

    feat: game text input and buttons (basic structure)

--- App.js
+++ App.js
@@ -1,75 +1,8 @@
-import { useState } from "react";
-import { StyleSheet, Text, View, FlatList, Button } from "react-native";
-import { StatusBar } from "expo-status-bar";
-
-import GoalItem from "./components/GoalItem";
-import GoalInput from "./components/GoalInput";
+import { StyleSheet } from "react-native";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  const [modalIsVisible, setModalIsVisible] = useState(false);
-  const [courseGoals, setCourseGoals] = useState([]);
-
-  function startAddGoalHandler() {
-    setModalIsVisible(true);
-  }
-
-  function endAddGoalHandler() {
-    setModalIsVisible(false);
-  }
-
-  function addGoalHandler(enteredGoalText) {
-    setCourseGoals((currentCourseGoals) => [
-      ...currentCourseGoals,
-      { text: enteredGoalText, id: Math.random().toString() }
-    ]);
-    endAddGoalHandler();
-  }
-
-  function deleteGoalHandler(id) {
-    setCourseGoals((currentCourseGoals) =>
-      currentCourseGoals.filter((goal) => goal.id !== id)
-    );
-  }
-
-  return (
-    <>
-      <StatusBar style="light" />
-      <View style={styles.appContainer}>
-        <Button
-          title="Add new goal"
-          color="#5E0acc"
-          onPress={startAddGoalHandler}
-        />
-        <GoalInput
-          visible={modalIsVisible}
-          onAddGoal={addGoalHandler}
-          onCancel={endAddGoalHandler}
-        />
-        <View style={styles.goalsContainer}>
-          <FlatList
-            data={courseGoals}
-            renderItem={({ item }) => (
-              <GoalItem
-                onDeleteItem={deleteGoalHandler}
-                text={item.text}
-                id={item.id}
-              />
-            )}
-            keyExtractor={(item, index) => item.id + index}
-          />
-        </View>
-      </View>
-    </>
-  );
+  return <StartGameScreen />;
 }
 
-const styles = StyleSheet.create({
-  appContainer: {
-    flex: 1,
-    paddingTop: 50,
-    paddingHorizontal: 16
-  },
-  goalsContainer: {
-    flex: 4
-  }
-});
+const styles = StyleSheet.create({});
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,44 @@
+import { TextInput, StyleSheet, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+      />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    backgroundColor: "#4e0329",
+    borderRadius: 8,
+    elevation: 8,
+    shadowColor: "black",
+    shadowOffset: { width: 2, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center"
+  }
+});
