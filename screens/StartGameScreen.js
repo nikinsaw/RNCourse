2379b86760d01fb94379dac6cf086407332e3305
nikinsaw <nikinsaw@gmail.com>
2024-07-25 17:27:37 +0530

    chore: add background image and linear gradient

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -9,16 +9,24 @@ function StartGameScreen() {
         maxLength={2}
         keyboardType="number-pad"
       />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
-  );
+  )
 }
 
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
     padding: 16,
     marginTop: 100,
     marginHorizontal: 24,
@@ -40,5 +48,11 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     fontWeight: "bold",
     textAlign: "center"
+  },
+  buttonsContainer: {
+    flexDirection: "row"
+  },
+  buttonContainer: {
+    flex: 1
   }
 });
